4c92dc6281598059c60f1aa153e25ec9cfbeffff
seong3890 <audtjddl100@naver.com>
2021-09-29 22:44:42 +0900

    Initial commit

---
+++ src/test/java/com/jojodu/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojodu.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
